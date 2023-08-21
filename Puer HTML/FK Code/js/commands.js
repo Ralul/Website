920221408bf7f9a1b63178b9e38ea992888f6a13
Ralul_888 <74320658+Ralul@users.noreply.github.com>
2023-08-21 13:00:28 +0200

    update

--- Puer HTML/FK Code/js/commands.js
+++ Puer HTML/FK Code/js/commands.js
@@ -84,3 +84,4 @@ banner = [
   "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
   '<span class="color2">Welcome to my interactive web terminal.</span>',
   "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
