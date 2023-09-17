4bbcfb7494cbeb58ffc41e6874f606b37b1cf8f0
Ralul_888 <74320658+Ralul@users.noreply.github.com>
2023-09-17 20:05:22 +0200

    console thing

---
+++ Puer HTML/Sandbox/app.js
@@ -0,0 +1,5 @@
+let items = ["Apple", "Banana", "Cherry", "Date", "Fig"];
+
+let output = items.join("<br>");
+
+document.getElementById("output").innerHTML = output;
--- Puer HTML/Scratch/main.js
+++ Puer HTML/Scratch/main.js
@@ -1,9 +1,37 @@
 let  inputBox = document.getElementById("myInput")
+const cachelist = []
 
-inputBox.addEventListener("input", function(){
+inputBox.addEventListener("keypress",function(event){
 
-    document.getElementById("output").innerHTML = inputBox.value
-    
-    console.log(document.getElementById(inputBox))
+    //Press Enter Key
+    if (event.key === "Enter"){
+        //Debug Stuff
+        console.log("PRESS ENTERs")
+            
+        //Add input string to Cachlist
+        cachelist.push(inputBox.value)
 
+        //Process to Line by Line Output
+        let terminalOutput = cachelist.join("<br>")
+
+        //Give the TerminalOutput to HTML output
+        document.getElementById("output").innerHTML = terminalOutput
+
+        //Debug Stuff
+        console.log(cachelist.join("\n"))
+
+        //Clear Input Field after Procesing
+        inputBox.value = ""
+
+    }
+    else if (event.key === "ArrowUp" || event.key === "ArrowDown"){
+        console.log("key up / down")
+    }    
 })
+
+// console up and donw key
+switch (event.key){
+
+}
+
+
