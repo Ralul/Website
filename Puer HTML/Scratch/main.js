d21ea26fa5568a1b295d81ec337f7208541e8041
Ralul_888 <74320658+Ralul@users.noreply.github.com>
2023-09-19 00:09:48 +0200

    Clipboardhystory mangement

--- Puer HTML/Scratch/main.js
+++ Puer HTML/Scratch/main.js
@@ -1,15 +1,20 @@
 let  inputBox = document.getElementById("myInput")
 const cachelist = []
+const clipboardHistory = []
+let index = 0
 
 inputBox.addEventListener("keypress",function(event){
 
     //Press Enter Key
-    if (event.key === "Enter"){
+    if (event.key === "Enter" && inputBox.value != ""){
         //Debug Stuff
         console.log("PRESS ENTERs")
             
         //Add input string to Cachlist
         cachelist.push(inputBox.value)
+
+        //Add input to begin to clipboardHistory 
+        clipboardHistory.unshift(inputBox.value)
 
         //Process to Line by Line Output
         let terminalOutput = cachelist.join("<br>")
@@ -23,6 +28,9 @@ inputBox.addEventListener("keypress",function(event){
         //Clear Input Field after Procesing
         inputBox.value = ""
 
+        //Index Clipboardhystory restet to zero
+        index = 0
+
     }
     else if (event.key === "ArrowUp" || event.key === "ArrowDown"){
         console.log("key up / down")
@@ -30,8 +38,32 @@ inputBox.addEventListener("keypress",function(event){
 })
 
 // console up and donw key
-switch (event.key){
+document.onkeydown = function(event) {
 
-}
+    if(clipboardHistory.length >= 0)
+
+        if(event.keyCode == 38 && index < clipboardHistory.length){
+            console.log("up")
+            inputBox.value = clipboardHistory[index]
+            console.log(index)
+            if(index < clipboardHistory.length-1){
+                index++
+            }
+        }
+
+        if(event.keyCode == 40 && index > 0){
+            if(index >= 0){
+                index--
+            }
+            console.log("down")
+            inputBox.value = clipboardHistory[index]
+
+            console.log(index)
+
+        }
+
+};
+
+   
 
 
